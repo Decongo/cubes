a50c09713c6507edd0debc34d72926d87a27cb6e
decongo <conger.devon@gmail.com>
2020-03-18 14:55:01 -0600

    finished implementation

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,39 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      
     </div>
     <router-view/>
   </div>
 </template>
+
+<script>
+import CubeCollection from './script'
+
+export default {
+  name: 'CubeCollection',
+
+  mounted() {
+    console.log(`started searching...`);
+    const start = Date.now();
+
+    let cubes = [['H','L','S','J','U','B'], 
+                 ['O','O','N','O','S','O'], 
+                 ['M','V','O','Y','A','O'], 
+                 ['E','E','W','' ,'' ,'' ]];
+    // let cubes = [['A',' ',' '],
+    //              ['D',' ',' '],
+    //              ['D',' ',' ']];
+    let cubeCollection = new CubeCollection();
+    cubeCollection.start(cubes);
+
+    const finished = Date.now();
+    console.log(`search finished in ${(finished - start) / 1000} seconds`);
+
+    console.log(cubeCollection.words);
+  }
+}
+</script>
 
 <style>
 #app {
---
+++ src/script.js
@@ -0,0 +1,63 @@
+// I'm doing this with the assumption that a space in the middle of a string invalidates the string
+
+import dictionary from './dictionary.txt'
+
+export default class CubeCollection {
+
+  constructor() {
+    this.words = [];
+  }
+
+  start(cubes=[]) {
+    this.arrangeCubes(cubes);
+  }
+
+  arrangeCubes(cubes=[], arrangedCubes=[]) {
+    if (!cubes.length) {
+      this.buildWord(arrangedCubes);
+      return;
+    }
+
+    let _cubes;
+    let _arrangedCubes;
+
+    let index;
+    cubes.forEach(cube => {
+      _cubes = cubes.slice();
+      _arrangedCubes = arrangedCubes.slice();
+
+      _arrangedCubes.push(cube);
+      index = _cubes.indexOf(cube);
+      _cubes.splice(index, 1);
+      this.arrangeCubes(_cubes, _arrangedCubes);
+    })
+  }
+
+  buildWord(cubes=[], word="") {
+    if (!cubes.length) {
+      if (!word.trim().length) return                    // handle words of no length
+      
+      if (this.words.includes(word.trim())) return;      // check for repeat
+
+      const regex = new RegExp(`\\b${word.trim().toLowerCase()}\\b`);
+      if (!regex.test(dictionary)) return;               // check dict
+
+      this.words.push(word.trim());
+      return;
+    }
+    
+    let _word;
+    let _cubes = cubes.slice();               // copy the cube array
+    const firstCube = _cubes.shift();         // pop the first cube
+
+    let usedLetters = [];
+
+    for (const letter of firstCube) {
+      if (usedLetters.includes(letter)) continue;
+
+      _word = word + letter;
+      usedLetters.push(letter);
+      this.buildWord(_cubes, _word);
+    }
+  }
+}
